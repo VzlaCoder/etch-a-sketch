94ba2b5011aad70f9b222397c7ecb9e1b98db014
Labboro <gtorrealba12@gmail.com>
2022-04-10 23:56:13 -0400

    Add a little more and yet nothing at all

--- interactivity.js
+++ interactivity.js
@@ -1,7 +1,15 @@
 const grid = document.getElementById('grid')
 
-function makeGrid(size)
+function makeGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('grid-element')
+        gridElement.style.color = 'green'
+        grid.appendChild(gridElement)
+    }
+}
 
 document.body.appendChild(grid)
